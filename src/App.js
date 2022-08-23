eab6d1e4fd5baab1fd1b90ca3068c481af7a3988
Mario <duydk@its-golbal.vn>
2022-08-23 20:45:36 +0700

    login-line

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 import React from 'react';
 
 function App() {
-
+  const code = new URL(window.location.href).searchParams.get("code");
+  const lineConfig = {
+    redirectURI: 'https://line-login-nine.vercel.app',
+    clientID: 1657409033,
+    state: 'b41c8fd15b895f0fc28bfwb9d7da89054d931e7s',
+    scope: 'profile%20openid',
+    nonce: 'd78a51235f6ee189e831q9c68523cfa336917ada'
+  }
+  console.log("🚀 ~ file: App.js ~ line 6 ~ App ~ code", code)
   const handleLineLogin = () => {
-    window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1657409033&redirect_uri=https://eco-api.its-globaltek.com&state=b41c8fd15b895f0fc28bfwb9d7da89054d931e7s&scope=profile%20openid&nonce=d78a51235f6ee189e831q9c68523cfa336917ada`
+    window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${lineConfig.clientID}&redirect_uri=${lineConfig.redirectURI}&state=${lineConfig.state}&scope=${lineConfig.scope}&nonce=${lineConfig.nonce}`
   }
   return (
     <div className='App'>
